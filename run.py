0f227237cee811a6d34f150feb3a6e432ef52f4f
Anestezia-zip <plekatybtc@gmail.com>
2023-09-24 21:47:49 +0100

    added function for inserting a column into a sheet

--- run.py
+++ run.py
@@ -45,6 +45,19 @@ def calculate_profit():
    
     return profit
 
+def update_worksheet_column(insert_column):
+    max_cols = max(len(row) for row in all_data)
+    num_rows = len(insert_column)
+
+    # Add the 'Profit' data array to the last column
+    for i in range(num_rows):
+        if i < len(all_data):
+            all_data[i].append(str(insert_column[i]))
+        else:
+            all_data.append([''] * (max_cols - 1) + [str(insert_column[i])])
+
+    # Updating the data in the sheet
+    sales.update('A1', all_data)
 
 def main():
     """ Run all program functions """
